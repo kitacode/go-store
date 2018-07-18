f63ea2f5a5c069f1488dc3333cf2990754596894
Irvan Maulana <irvanmaulana2903@gmail.com>
2018-07-18 09:27:54 +0700

    add .travis

--- shopify/shopify.go
+++ shopify/shopify.go
@@ -8,35 +8,21 @@ import (
 
 type Shopify struct {
     store.Store
-    Order
     Apikey        string
     ApiPassword   string
 }
 
-func (shopify *Shopify) GetOrders() {
+func (shopify *Shopify) GetOrders() Orders {
   url := shopify.buildUrl()
   json_str := shopify.Get(url)
 
-  // rawIn := json.RawMessage(json_str)
-  // orders, err := rawIn.MarshalJSON()
-  // if err != nil {
-  //     panic(err)
-  // }
-  //
-  // var o Orders
-  // err = json.Unmarshal(orders, &o)
-  // if err != nil {
-  //     panic(err)
-  // }
-  //
-  // a, _ := json.Marshal(o)
-  //
-  // fmt.Println(string(a))
-
   var orders Orders
   json.Unmarshal([]byte(json_str), &orders)
-  a, _ := json.Marshal(orders)
-  fmt.Println(string(a))
+  // a, _ := json.Marshal(orders)
+  // fmt.Println(string(a))
+
+  // return string(a)
+  return orders
 }
 
 func (shopify *Shopify) GetOrder() {
